2ae4b7aa1e8390d68d9f1014c3f0d670c8c3c167
yash <ym130602@gmail.com>
2026-03-30 10:56:19 +0530

    try-1

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   outputFileTracingIncludes: {
-    '/**': ['./lib/generated/prisma/libquery_engine-rhel-openssl-3.0.x.so.node'],
+    '/*': ['./lib/generated/prisma/**/*'],
   },
-  serverExternalPackages: ['@prisma/client'],
 };
 
 export default nextConfig;
