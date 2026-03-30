955174703323da3d21ac6dca469dcdaf821931ee
yash <ym130602@gmail.com>
2026-03-30 11:24:15 +0530

    added prism to dependency

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   outputFileTracingIncludes: {
-    '/*': ['./lib/generated/prisma/**/*'],
+    "/**": ["./lib/generated/prisma/**/*"],
   },
 };
 
